166387423958e7ac034dec98701c3595528e17c2
m00nLeo <tranlehoanglong2001@gmail.com>
2023-02-03 01:14:24 +0700

    feat. update Final admin

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import router from "./router";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import axios from "axios";
+import App from "./App";
 
 axios.defaults.baseURL = "https://server-hotel.onrender.com";
 
@@ -15,7 +16,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     {/* Reac - Query */}
     <QueryClientProvider client={queryClient}>
       {/* React-router-dom */}
-      <RouterProvider router={router} />
+      {/* <RouterProvider router={router} /> */}
+      <App />
     </QueryClientProvider>
   </React.StrictMode>
 );
